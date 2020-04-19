dcca3adb2676274d802980605557bff8fff3cc5e
OanhPham1903 <oanhphampt1903@gmail.com>
2020-04-19 23:13:26 +0700

    1 file modified, 6 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { AboutComponent } from './about/about.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, AboutComponent, FooterComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
